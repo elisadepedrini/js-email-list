84f54483f548d0548b2031111ab0cc93ff1b6ec8
elisadepedrini <elisa.depedrini@gmail.com>
2026-04-01 16:22:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Descrizione
+
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+
+- Abbellire con CSS o Bootstrap
+- Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre) */
+
+
+
+const ulEl = document.querySelector('ul')
+
+
+for (let i = 0; i < 10; i++) {
+    
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(response => response.json())
+    .then(data => {
+        const email = data.response
+
+        const listEl = document.createElement('li')
+        ulEl.appendChild(listEl)
+        listEl.innerHTML = email
+    }  
+)}
